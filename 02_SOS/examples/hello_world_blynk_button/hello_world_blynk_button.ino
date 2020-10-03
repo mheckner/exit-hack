7ca1adab2f18001d6d5891937966e957aa5fb67d
mheckner <markus.heckner@oth-regensburg.de>
2020-10-03 16:10:53 +0200

    added fritz sketch for blynk

---
+++ 02_SOS/examples/hello_world_blynk_button/hello_world_blynk_button.ino
@@ -0,0 +1,15 @@
+int LED_PIN = 12;
+int BUTTON_PIN = 2;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop() {
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+}
