ad7f1297acba4b37db3973bab0a7a7b10b87ce2a
mheckner <markus.heckner@oth-regensburg.de>
2020-10-08 08:38:17 +0200

    added starter sos

--- 02_SOS/solution/sos/sos.ino
+++ 02_SOS/solution/sos/sos.ino
@@ -1,12 +1,23 @@
 /*
-  SOS
-  Sends an SOS signal with Arduino.
+  Blink
+  Lights an LED, when a button is pressed.
+  Use this example as a starting point for SOS.
 */
+
+int LED_PIN = 12;
+int BUTTON_PIN = 2;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
 }
